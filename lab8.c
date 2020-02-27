e32e798e3e1d1a4d27cd23ceb83b049e4e8c605d
Andrii-I <ieroshenkoa@gmail.com>
2020-02-27 13:11:19 -0800

    WIP

--- lab8.c
+++ lab8.c
@@ -22,6 +22,11 @@ typedef struct mypipe
     int end_occupied;
 }mypipe;
 
+int min(int x, int y) 
+{ 
+    return y ^ ((x ^ y) & -(x < y)); 
+} 
+
 //initializes (malloc) the pipe with a size of "size" and sets start and end.
 void init_pipe(mypipe* pipe, int size)
 {
@@ -43,34 +48,79 @@ int mywrite(mypipe* pipe, byte* buffer, int size)
     }
     else
     {
-        int i = pipe->start_occupied;
+        int occupied;
+        if (pipe->start_occupied == pipe->end_occupied)
+        {
+            occupied = 0;
+        }
+        else if (pipe->start_occupied < pipe->end_occupied)
+        {
+            occupied = pipe->end_occupied - pipe->start_occupied + 1;
+        }
+        else
+        {
+            occupied = pipe->buffersize - pipe->start_occupied + pipe->end_occupied;
+        }
+        
+
+        int i; 
+        if ( occupied == 0)
+        {
+            i = 0;
+            pipe->start_occupied = i;
+        }
+        else if (occupied + size > pipe->buffersize) 
+        {
+            i = pipe->start_occupied = pipe->end_occupied + 1;
+        }
+        else 
+        {
+            i = pipe->end_occupied + 1;
+        }
+
+        
 
         for (int j = 0; j < size; j++)
         {
             pipe->pipebuffer[i] = buffer[j];
+            pipe->end_occupied = i;
             i++;
-            
-            if (i > size - 1)
+            if (i == pipe->buffersize)
             {
                 i = 0;
-                //MODIFY start & end occupied in a special way
-            }
-            else 
-            {
-                //standard start & end modification
-            
             }
         }
-    }
 
-    //change later to size written
-    return 0;
+        return size;
+    }
 }
 
 //reads "size" bytes from pipe into buffer, returns how much it read (max size), 0 if pipe is empty
 int myread(mypipe* pipe, byte* buffer, int size)
 {
     //In normal pipes, lets say you have "hello", 6 bytes, but you read 60. read still returns 6, because there was not more in the pipe
+    
+    int to_read;
+
+    if (pipe->start_occupied == pipe->end_occupied) return 0;
+    else if (pipe->end_occupied > pipe->start_occupied)
+    {
+        to_read = min(size, pipe->end_occupied - pipe->start_occupied + 1);      
+    }
+    else
+    {
+        to_read = min(size, pipe->buffersize - pipe->start_occupied + pipe->end_occupied);
+    }
+
+    int i = pipe->start_occupied;
+    for (int j = 0; j < to_read; j++)
+    {
+        buffer[j] = pipe->pipebuffer[i];
+        i++;
+    }
+
+    return to_read;
+
 }
 
 void main()
@@ -82,17 +132,34 @@ void main()
     
     init_pipe(&pipeA, 32);
     mywrite(&pipeA, "hello world", 12);
+
+     for (int i = 0; i < 32; i++)
+    {
+        printf("%c\n", pipeA.pipebuffer[i]);
+    }
+
+    printf("**********************\n"); 
+
+
     mywrite(&pipeA, "it's a nice day", 16);
-    strcpy(text, pipeA.pipebuffer);
-    printf("%s\n", text);
-    
-/*     myread(&pipeA, text, 12);
-    printf("%s\n", text);
+
+    for (int i = 0; i < 32; i++)
+    {
+        printf("%c\n", pipeA.pipebuffer[i]);
+    } 
+
+    myread(&pipeA, text, 12);
+    printf("1: %s\n", text);
     
     myread(&pipeA, text, 16);
-    printf("%s\n", text);
+    printf("2: %s\n", text);
     
     mywrite(&pipeA, "and now we test the carryover", 30);
     myread(&pipeA, text, 16);
-    printf("%s\n", text); */
+    printf("3: %s\n", text);
+
+    for (int i = 0; i < 32; i++)
+    {
+        printf("%c\n", pipeA.pipebuffer[i]);
+    } 
 }
