c40cd9aaa8817c70ca6f67ee475364e4815679da
Andrii-I <ieroshenkoa@gmail.com>
2020-02-27 00:48:08 -0800

    WIP2

--- lab8.c
+++ lab8.c
@@ -16,16 +16,17 @@ typedef unsigned char byte;
 
 typedef struct mypipe
 {
-byte* pipebuffer;
-int buffersize;
-int start_occupied;
-int end_occupied;
+    byte* pipebuffer;
+    int buffersize;
+    int start_occupied;
+    int end_occupied;
 }mypipe;
 
 //initializes (malloc) the pipe with a size of "size" and sets start and end.
 void init_pipe(mypipe* pipe, int size)
 {
-    pipe->pipebuffer = (byte*)malloc(size);
+    byte* t = (byte*)malloc(size);
+    pipe->pipebuffer = t;
     pipe->buffersize = size;
     pipe->start_occupied = pipe->end_occupied = 0;
 
@@ -35,17 +36,30 @@ void init_pipe(mypipe* pipe, int size)
 //writes "size" bytes from buffer into the pipe, returns size
 int mywrite(mypipe* pipe, byte* buffer, int size)
 {
-    //It returns how many bytes you have written into the pipe. That sounds redundant, but it could be less than the actual size if the pipe is smaller.
-    if (size > pipe->buffersize)
+    //It returns how many bytes you have written into the pipe. That sounds redundant, but it could be less than the actual size if the pipe is smaller.     
+    if (size > pipe->buffersize || size <= 0)
     {
         return 0;
     }
     else
     {
-        for (int i = 0; i < size; i++)
+        int i = pipe->start_occupied;
+
+        for (int j = 0; j < size; j++)
         {
-            pipe->pipebuffer[pipe->start_occupied + i] = buffer[i];
-            //add overflow stuff
+            pipe->pipebuffer[i] = buffer[j];
+            i++;
+            
+            if (i > size - 1)
+            {
+                i = 0;
+                //MODIFY start & end occupied in a special way
+            }
+            else 
+            {
+                //standard start & end modification
+            
+            }
         }
     }
 
@@ -61,24 +75,24 @@ int myread(mypipe* pipe, byte* buffer, int size)
 
 void main()
 {
-
     //TESTING CODE:
 
     char text[100];
-    mypipe* pipeA;
+    mypipe pipeA;
     
-    init_pipe(pipeA, 32);
-    printf("piped\n");
-    mywrite(pipeA, "hello world", 12);
-    mywrite(pipeA, "it's a nice day", 16);
-    printf("written\n");
-
-    myread(pipeA, text, 12);
+    init_pipe(&pipeA, 32);
+    mywrite(&pipeA, "hello world", 12);
+    mywrite(&pipeA, "it's a nice day", 16);
+    strcpy(text, pipeA.pipebuffer);
     printf("%s\n", text);
-    myread(pipeA, text, 16);
+    
+/*     myread(&pipeA, text, 12);
     printf("%s\n", text);
-
-    /*mywrite(pipeA, "and now we test the carryover", 30);
-    myread(pipeA, text, 30);
-    printf("%s\n", text);*/
+    
+    myread(&pipeA, text, 16);
+    printf("%s\n", text);
+    
+    mywrite(&pipeA, "and now we test the carryover", 30);
+    myread(&pipeA, text, 16);
+    printf("%s\n", text); */
 }
